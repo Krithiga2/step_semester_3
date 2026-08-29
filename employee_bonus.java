ec4c2db6614f2f9cf9a0c2386cfadd918d86485f
krithiga <krithigashree804@gmail.com>
2026-08-29 11:47:54 +0530

    Update m2.java

--- employee_bonus.java
+++ employee_bonus.java
@@ -15,12 +15,7 @@ class Employee {
 
 public class employee_bonus {
     public static void main(String[] args) {
-        Employee[] employees = {
-            new Employee("E-101", 40000),
-            new Employee("E-102", 55000),
-            new Employee("E-103", 62000),
-            new Employee("E-104", 48000)
-        };
+        Employee[] employees = {new Employee("E-101", 40000),new Employee("E-102", 55000),new Employee("E-103", 62000),new Employee("E-104", 48000)};
         double bonus = 5000;
         for (Employee employee : employees) {
             employee.raiseSalary(bonus);
